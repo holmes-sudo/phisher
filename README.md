9ccaec04861f7eb59c090fadb8e0027fdef34ea8
Holmes <401m35h3r10ck@gmail.com>
2025-06-05 00:27:11 +0100

    Removed all the uncessary files.

--- README.md
+++
@@ -1,2 +0,0 @@
-This is by far the simplest phishing tool out there.It's easy to use and tweak.
- 
--- testing.py
+++
@@ -1,3 +0,0 @@
-import requests
-response = requests.get("http://localhost:3000/users")  # Your local API
-print(response.json())  # Your server's response
